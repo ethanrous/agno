2736ccbd6f463586b0bc71899f7c5fef5f840f41
EthRousseau <ethanrousseau99+github@gmail.com>
2026-03-27 13:31:05 -0400

    feat: add page_count field to AgnoImage struct and C header
    Add `page_count: u64` to `AgnoImage` between `height` and `exif` (required
    for #[repr(C)] FFI field order). Default to 1 in both constructors. Add
    `load_pdf_page` declaration to `lib/agno.h`.

--- agno/src/agno_image/image.rs
+++ agno/src/agno_image/image.rs
@@ -11,6 +11,7 @@ pub struct AgnoImage {
 
     pub width: u64,
     pub height: u64,
+    pub page_count: u64,
 
     pub exif: ExifContext,
 }
@@ -25,6 +26,7 @@ impl AgnoImage {
                 len: 0,
                 height: 0,
                 width: 0,
+                page_count: 1,
             };
         }
 
@@ -38,6 +40,7 @@ impl AgnoImage {
             len: data.len(),
             height,
             width,
+            page_count: 1,
         }
     }
 
